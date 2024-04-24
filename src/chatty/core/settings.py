7dfa684b40e0fdba7bfdb54d0883a9cd33fecf41
arifgarayev <garayevarif@gmail.com>
2024-04-24 13:40:30 +0400

    designing auth feature

--- src/chatty/core/settings.py
+++ src/chatty/core/settings.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import os
-# load the .env file
 from dotenv import load_dotenv
 
 # call the load_dotenv() function
 load_dotenv('../../.env')
 
 ENV_PREFIX = "DJANGO_"
+DATABASE_PREFIX = "POSTGRES_"
 
 # define the BASE_DIR for PYTHONPATH
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,16 +20,32 @@ print(SECRET_KEY)
 # load from the environment variables
 try:
     DEBUG = bool(os.getenv(f"{ENV_PREFIX}IS_DEBUG"))
+
+
 except ValueError:
     DEBUG = False
+    
+
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOW_CREDENTIALS = True
+else:
+    CORS_ALLOWED_ORIGINS = [
+        ...
+]
 
 
-# no host, only local development
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+# no domain-name, only local development
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'nginx']
+
+
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -37,6 +53,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # security 
+    "corsheaders",
+
+    # debugging
+    "debug_toolbar",
+
+    # rest
+    "rest_framework"
+
+
 ]
 
 MIDDLEWARE = [
@@ -47,9 +74,19 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    # additional middlewares
+    "corsheaders.middleware.CorsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
+# signed cookies enabled
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+SESSION_COOKIE_HTTPONLY = True
+
+# root url conf module
 ROOT_URLCONF = "core.urls"
+
 
 TEMPLATES = [
     {
@@ -70,20 +107,20 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Database config
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv(f"{DATABASE_PREFIX}DB"),
+        "USER": os.getenv(f"{DATABASE_PREFIX}USER"),
+        "PASSWORD": os.getenv(f"{DATABASE_PREFIX}PASSWORD"),
+        "HOST": os.getenv(f"127.0.0.1"),
+        "PORT": os.getenv(f"5432")
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -101,23 +138,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+TIME_ZONE = "Asia/Baku"
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# config pagination rules
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30
+}
