c370c148e5fa4dca23a1ab7ffacb6130a65799fa
Kenan Garayev <kenangarayev@gmail.com>
2024-06-15 21:46:05 +0100

    docker compose changed

--- init-db.sh
+++ init-db.sh
@@ -4,5 +4,4 @@ set -e
 # Create pg_notify directory if it doesn't exist
 mkdir -p /var/lib/postgresql/data/pg_notify
 
-# Execute the original entrypoint script
-exec docker-entrypoint.sh "$@"
+# Your other initialization commands here, if any
